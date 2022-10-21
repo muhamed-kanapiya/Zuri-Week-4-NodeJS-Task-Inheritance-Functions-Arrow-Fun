5bfbcfa8c5914848b38841555cc03d497c5646be
Muhamed-Kanapiya <muhamed08@mail.ru>
2022-10-21 20:50:24 +0000

    all

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ console.log(n);
 let guess = prompt(name + " Welcome" + " to stage one. Guess a number between 1 and 2")
 
 if (guess == n) {
-  alert("correct. you have earned 10 points");
-  var p = Math.random();
+  alert("correct. you have earned 7 points");
+  let p = Math.random();
   p = p * 3;
   p = Math.floor(p) + 1;
   console.log(p);
   let guess2 = prompt(name + " Welcome" + " to stage two Guess the Number in my console to finish game. hint: The number is ranging from 1 to 3");
   if (guess2 == p) {
-    alert("Congrats " + name + " You finished the game, and received 20 points")
+    alert("Congrats " + name + " You finished the game, and received 14 points")
   } else { alert("I think you need a reboost, Try again") }
 
-} else { alert("I thought You were a genius lol, try again") }
+} else { alert("It was easy, try again") }
